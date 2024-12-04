68a1d885ca534e21668506eb06b7b2bb9bf2d51f
YuxuanZhao23 <yuxuan_zhao@alumni.brown.edu>
2024-12-03 19:53:47 -0500

    refactor

--- linear.py
+++ linear.py
@@ -1,22 +1,21 @@
 import torch
-from re import L
 from torch import nn
 from torch.utils import data
 
 # data
 def create_synthetic_data(w, b, num_examples):
-  X = torch.normal(0, 1, (num_examples, len(w))) # 定义均值为0，方差为1，大小为样本数 x 每个样本的（这里可以是随机选）
-  y = torch.matmul(X, w) + b
-  y += torch.normal(0, 0.01, y.shape) # 加入噪音均值为0，方差为0.01（噪音一般用正态分布）
-  return X, y.reshape((-1, 1)) # 把 y 变成列向量
+    X = torch.normal(0, 1, (num_examples, len(w))) # 定义均值为0，方差为1，大小为样本数 x 每个样本的（这里可以是随机选）
+    y = torch.matmul(X, w) + b
+    y += torch.normal(0, 0.01, y.shape) # 加入噪音均值为0，方差为0.01（噪音一般用正态分布）
+    return X, y.reshape((-1, 1)) # 把 y 变成列向量
 
 true_w = torch.tensor([2, -3.4])
 true_b = torch.tensor(4.2)
 features, labels = create_synthetic_data(true_w, true_b, 1000)
 
 def load_array(features, labels, batch_size, is_train=True):
-  dataset = data.TensorDataset(features, labels)
-  return data.DataLoader(dataset, batch_size, shuffle=is_train)
+    dataset = data.TensorDataset(features, labels)
+    return data.DataLoader(dataset, batch_size, shuffle=is_train)
 
 # batch size 其实比较小是一件好事，因为越小噪音越多，反而避免一开始越走越歪
 batch_size = 100
@@ -33,11 +32,11 @@ trainer = torch.optim.SGD(net.parameters(), lr=lr)
 
 # train
 for epoch in range(num_epochs):
-  for X, y in data_iter:
-    l = loss(net(X), y) # 正着算 loss
-    trainer.zero_grad()
-    l.backward() # l是长度为1的tensor，会更新w
-    trainer.step()
+    for X, y in data_iter:
+        l = loss(net(X), y) # 正着算 loss
+        trainer.zero_grad()
+        l.backward() # l是长度为1的tensor，会更新w
+        trainer.step()
 
-  l = loss(net(features), labels)
-  print(f'epoch {epoch + 1}, loss {l:f}')
+    l = loss(net(features), labels)
+    print(f'epoch {epoch + 1}, loss {l:f}')
---
+++ utils/utils.py
@@ -0,0 +1,78 @@
+from utils.animator import Animator
+import torch
+import torchvision
+from torch.utils import data
+from torchvision import transforms
+from torch import nn
+
+class Accumulator:
+    def __init__(self, n): self.data = [0.0] * n
+
+    def add(self, *args): self.data = [a + float(b) for a, b in zip(self.data, args)]
+
+    def reset(self): self.data = [0.0] * len(self.data)
+
+    def __getitem__(self, idx): return self.data[idx]
+
+def load_data_fashion_mnist(batch_size, resize=None):
+    trans = [transforms.ToTensor()]
+    if resize: trans.insert(0, transforms.Resize(resize))
+    trans = transforms.Compose(trans)
+    # 这里的 transform=trans 是指把图片变成 tensor
+    mnist_train = torchvision.datasets.FashionMNIST(root="../data", train=True, transform=trans, download=True)
+    mnist_test = torchvision.datasets.FashionMNIST(root="../data", train=False, transform=trans, download=True)
+    return (data.DataLoader(mnist_train, batch_size, shuffle=True, num_workers=4),
+            data.DataLoader(mnist_test, batch_size, shuffle=False, num_workers=4))
+
+def init_weights(m):
+    if type(m) == nn.Linear: nn.init.normal_(m.weight, std=0.01)
+
+def accuracy(y_hat, y):
+    if len(y_hat.shape) > 1 and y_hat.shape[1] > 1: y_hat = y_hat.argmax(axis=1)
+    cmp = y_hat.type(y.dtype) == y # 正确的数量
+    return float(cmp.type(y.dtype).sum())
+
+def evaluate_accuracy(net, data_iter):
+    """计算在指定数据集上模型的精度"""
+    if isinstance(net, torch.nn.Module):
+        net.eval()  # 将模型设置为评估模式
+    metric = Accumulator(2)  # 正确预测数、预测总数
+    with torch.no_grad():
+        for X, y in data_iter:
+            metric.add(accuracy(net(X), y), y.numel())
+    return metric[0] / metric[1]
+
+def train_epoch_ch3(net, train_iter, loss, updater):
+    """训练模型一个迭代周期（定义见第3章）"""
+    # 将模型设置为训练模式
+    if isinstance(net, torch.nn.Module): net.train()
+    # 训练损失总和、训练准确度总和、样本数
+    metric = Accumulator(3)
+    for X, y in train_iter:
+        # 计算梯度并更新参数
+        y_hat = net(X)
+        l = loss(y_hat, y)
+        if isinstance(updater, torch.optim.Optimizer):
+            # 使用PyTorch内置的优化器和损失函数
+            updater.zero_grad()
+            l.mean().backward()
+            updater.step()
+        else:
+            # 使用定制的优化器和损失函数
+            l.sum().backward()
+            updater(X.shape[0])
+        metric.add(float(l.sum()), accuracy(y_hat, y), y.numel())
+    # 返回训练损失和训练精度
+    return metric[0] / metric[2], metric[1] / metric[2]
+
+def train_ch3(net, train_iter, test_iter, loss, num_epochs, updater):
+    animator = Animator(xlabel='epoch', xlim=[1, num_epochs], ylim=[0.3, 0.9],
+                        legend=['train loss', 'train acc', 'test acc'])
+    for epoch in range(num_epochs):
+        train_metrics = train_epoch_ch3(net, train_iter, loss, updater)
+        test_acc = evaluate_accuracy(net, test_iter)
+        animator.add(epoch + 1, train_metrics + (test_acc,))
+    # train_loss, train_acc = train_metrics
+    # assert train_loss < 0.5, train_loss
+    # assert train_acc <= 1 and train_acc > 0.7, train_acc
+    # assert test_acc <= 1 and test_acc > 0.7, test_acc
