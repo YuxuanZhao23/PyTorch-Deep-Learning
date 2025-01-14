95118786d3fce45d3ef3b64e98ed86826c08b990
YuxuanZhao23 <yuxuan_zhao@alumni.brown.edu>
2025-01-13 21:07:29 -0500

    refactor

--- gpt2.py
+++ gpt2.py
@@ -137,50 +137,68 @@ class GPT(nn.Module):
 
         return model
 
+def generate_five_examples(device):
+
+    model = GPT.from_pretrained('gpt2')
+    model.eval()
+    model.to(device)
+
+    num_return_sequences = 5
+    max_length = 30
+    tokens = enc.encode("Hello, I'm a language model,")
+    tokens = torch.tensor(tokens, dtype=torch.long) # (8,)
+    tokens = tokens.unsqueeze(0).repeat(num_return_sequences, 1) # (5, 8)
+    x = tokens.to(device)
+    torch.manual_seed(42)
+    torch.cuda.manual_seed(42)
+
+    while x.size(1) < max_length:
+        # forward the model to get the logits
+        with torch.no_grad():
+            logits = model(x) # (B, T, vocab_size)
+            # take the logits at the last position
+            logits = logits[:, -1, :] # (B, vocab_size)
+            # get the probabilities
+            probs = F.softmax(logits, dim=-1)
+            # do top-k sampling of 50 (huggingface pipeline default)
+            # topk_probs here becomes (5, 50), topk_indices is (5, 50)
+            topk_probs, topk_indices = torch.topk(probs, 50, dim=-1) # 这就是 beam search
+            # Renormalize probabilities
+            topk_probs = topk_probs / topk_probs.sum(dim=-1, keepdim=True)
+            # select a token from the top-k probabilities
+            # note: multinomial does not demand the input to sum to 1
+            ix = torch.multinomial(topk_probs, 1) # (B, 1)
+            # gather the corresponding indices
+            xcol = torch.gather(topk_indices, -1, ix) # (B, 1)
+            # append to the sequence
+            x = torch.cat((x, xcol), dim=1)
+
+    for i in range(5):
+        tokens = x[i, :max_length].tolist()
+        decoded = enc.decode(tokens)
+        print(">", decoded)
+
+def train():
+    with open('input.txt', 'r') as f:
+        text = f.read()
+    text = text[:1000]
+    tokens = enc.encode(text)
+    B, T = 4, 32
+    buffer = torch.tensor(tokens[: B * T + 1])
+    x = buffer[:-1].view(B, T)
+    y = buffer[1:].view(B, T)
+
+    model = GPT(GPTConfig())
+    model.to(device)
+    logits = model(x)
+    print(logits.shape)
+
+import tiktoken
+enc = tiktoken.get_encoding('gpt2')
 device = 'cpu'
 if torch.cuda.is_available(): device = 'cuda'
 elif hasattr(torch.backends, 'mps') and torch.backends.mps.is_available(): device = 'mps'
 print(f'using device: {device}')
 
-num_return_sequences = 5
-max_length = 30
-
-model = GPT.from_pretrained('gpt2')
-# model = GPT(GPTConfig())
-model.eval()
-model.to(device)
-
-import tiktoken
-enc = tiktoken.get_encoding('gpt2')
-tokens = enc.encode("Hello, I'm a language model,")
-tokens = torch.tensor(tokens, dtype=torch.long) # (8,)
-tokens = tokens.unsqueeze(0).repeat(num_return_sequences, 1) # (5, 8)
-x = tokens.to(device)
-torch.manual_seed(42)
-torch.cuda.manual_seed(42)
-
-while x.size(1) < max_length:
-    # forward the model to get the logits
-    with torch.no_grad():
-        logits = model(x) # (B, T, vocab_size)
-        # take the logits at the last position
-        logits = logits[:, -1, :] # (B, vocab_size)
-        # get the probabilities
-        probs = F.softmax(logits, dim=-1)
-        # do top-k sampling of 50 (huggingface pipeline default)
-        # topk_probs here becomes (5, 50), topk_indices is (5, 50)
-        topk_probs, topk_indices = torch.topk(probs, 50, dim=-1) # 这就是 beam search
-        # Renormalize probabilities
-        topk_probs = topk_probs / topk_probs.sum(dim=-1, keepdim=True)
-        # select a token from the top-k probabilities
-        # note: multinomial does not demand the input to sum to 1
-        ix = torch.multinomial(topk_probs, 1) # (B, 1)
-        # gather the corresponding indices
-        xcol = torch.gather(topk_indices, -1, ix) # (B, 1)
-        # append to the sequence
-        x = torch.cat((x, xcol), dim=1)
-
-for i in range(5):
-    tokens = x[i, :max_length].tolist()
-    decoded = enc.decode(tokens)
-    print(">", decoded)
+train()
+# generate_five_examples(device)
