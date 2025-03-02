7a6661d22e123deac6cef6ff09bec77a04c002c0
YuxuanZhao23 <yuxuan_zhao@alumni.brown.edu>
2025-03-02 11:06:41 -0500

    refactor

--- xtd/transform.py
+++
@@ -1,9 +0,0 @@
-import cv2
-from torchvision import transforms
-
-img = cv2.imread(r'xtd\hymenoptera\train\ants\512164029_c0a66b8498.jpg')
-
-tt = transforms.ToTensor()
-t = tt(img)
-
-print(t)
---
+++ xtd/transforms.py
@@ -0,0 +1,19 @@
+import cv2
+from torchvision import transforms
+from torch.utils.tensorboard import SummaryWriter
+
+img = cv2.imread(r'xtd\hymenoptera\train\ants\512164029_c0a66b8498.jpg')
+
+toTensor = transforms.ToTensor()
+toNormalize = transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])
+toCrop = transforms.RandomCrop(256)
+toResize = transforms.Resize(128)
+tran = transforms.Compose([toTensor, toNormalize, toCrop, toResize])
+
+sw = SummaryWriter('logs')
+
+for i in range(10):
+    sw.add_image("composed", tran(img), i)
+
+sw.flush()
+sw.close()
