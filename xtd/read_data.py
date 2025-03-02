8645b9c3cdc302f87b6ef366f67c0e599331bf08
YuxuanZhao23 <yuxuan_zhao@alumni.brown.edu>
2025-03-02 01:16:18 -0500

    refactor

---
+++ xtd/read_data.py
@@ -0,0 +1,27 @@
+from torch.utils.data import Dataset
+import os
+import cv2
+
+class HymenoteraDataSet(Dataset):
+    def __init__(self, path, label):
+        self.path = path
+        self.label = label
+        self.imageListPath = os.listdir(os.path.join(path, label))
+
+    def __getitem__(self, index):
+        path = os.path.join(self.path, self.label, self.imageListPath[index])
+        img = cv2.imread(path)
+        self.showImage(img)
+        return img
+    
+    def __len__(self):
+        return len(self.imageListPath)
+    
+    def showImage(self, image):
+        cv2.imshow(self.label, image)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+    
+hda = HymenoteraDataSet('xtd\\hymenoptera\\train', 'ants')
+hdb = HymenoteraDataSet('xtd\\hymenoptera\\train', 'bees')
+hda.__getitem__(0)
---
+++ xtd/test_tensorboard.py
@@ -0,0 +1,9 @@
+from torch.utils.tensorboard import SummaryWriter
+
+writer = SummaryWriter('logs')
+
+for i in range(100):
+    writer.add_scalar("y = 3x", 3*i, i)
+
+writer.flush()
+writer.close()
