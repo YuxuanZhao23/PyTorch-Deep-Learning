00fdf6b6b6377b1ef331f426ff5120ecd4facaca
YuxuanZhao23 <yuxuan_zhao@alumni.brown.edu>
2025-03-03 14:09:42 -0500

    accuracy tensorboard

--- xtd/test_vgg.py
+++ xtd/test_vgg.py
@@ -1,23 +1,29 @@
+from torch import no_grad
 from torch.utils.data import DataLoader
 from torchvision.datasets import CIFAR10
 from torchvision.transforms import ToTensor
 from torch.nn import Module, Conv2d, MaxPool2d, ReLU, Sequential, Flatten, Linear, Dropout, CrossEntropyLoss
 from torch.nn.init import xavier_uniform_
 from torch.utils.tensorboard import SummaryWriter
-from torch.optim import SGD
-from torch import no_grad
+from torch.optim import Adam
+
+# lr = 5e-2 # SGD
+lr = 1e-3 # Adam
+batch_size = 64
+num_epochs = 50
+loss_function = CrossEntropyLoss()
 
 class Block(Module):
     def __init__(self, num_convs, in_channels, out_channels):
         super().__init__()
-        network = []
-        network.append(Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=3, stride=1, padding=1))
-        network.append(ReLU())
-        for _ in range(num_convs-1):
-            network.append(Conv2d(in_channels=out_channels, out_channels=out_channels, kernel_size=3, stride=1, padding=1))
-            network.append(ReLU())
-        network.append(MaxPool2d(kernel_size=2, stride=2))
-        self.network = Sequential(*network)
+        layers = []
+        layers.append(Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=3, stride=1, padding=1))
+        layers.append(ReLU())
+        for _ in range(num_convs - 1):
+            layers.append(Conv2d(in_channels=out_channels, out_channels=out_channels, kernel_size=3, stride=1, padding=1))
+            layers.append(ReLU())
+        layers.append(MaxPool2d(kernel_size=2, stride=2))
+        self.network = Sequential(*layers)
 
     def forward(self, x):
         return self.network(x)
@@ -25,63 +31,70 @@ class Block(Module):
 class VGG(Module):
     def __init__(self):
         super().__init__()
-        self.network = Sequential(Block(1, 3, 64), Block(2, 64, 128), Flatten(),
-                                   Linear(128 * 8 * 8, 4096), ReLU(), Dropout(0.5),
-                                   Linear(4096, 10))
+        self.network = Sequential(
+            Block(1, 3, 64),
+            Block(2, 64, 128),
+            Flatten(),
+            Linear(128 * 64, 4096),
+            ReLU(),
+            Dropout(0.5),
+            Linear(4096, 10)
+        )
         self.network.apply(self.init_weights)
         self.network.to(device)
 
     def forward(self, x):
         return self.network(x)
-    
+
     def init_weights(self, model):
-        if type(model) == Linear or type(model) == Conv2d:
+        if isinstance(model, (Linear, Conv2d)):
             xavier_uniform_(model.weight)
     
-def accuracy(y_hat, y):
-    if len(y_hat.shape) > 1 and y_hat.shape[1] > 1: y_hat = y_hat.argmax(axis=1)
-    cmp = y_hat.type(y.dtype) == y
-    return float(cmp.type(y.dtype).sum())
+def accuracy(outputs, targets):
+    if outputs.dim() > 1 and outputs.size(1) > 1:
+        outputs = outputs.argmax(dim=1)
+    return (outputs == targets).sum().item()
 
-train_data_set = CIFAR10('./CIFAR10', train=True, transform=ToTensor())
-test_data_set = CIFAR10('./CIFAR10', train=False, transform=ToTensor())
-train_data_loader = DataLoader(dataset=train_data_set, batch_size=64)
-test_data_loader = DataLoader(dataset=test_data_set, batch_size=64)
+train_dataset = CIFAR10('./CIFAR10', train=True, transform=ToTensor(), download=True)
+test_dataset = CIFAR10('./CIFAR10', train=False, transform=ToTensor(), download=True)
+train_loader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
+test_loader = DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=False)
 device = 'cuda'
 writer = SummaryWriter('logs')
 vgg = VGG()
-optimizer = SGD(vgg.parameters(), lr=5e-2)
-loss = CrossEntropyLoss()
-num_of_batch = len(train_data_loader)
+optimizer = Adam(vgg.parameters(), lr=lr)
+num_batches = len(train_loader)
 
-for epoch in range(25):
+for epoch in range(num_epochs):
     vgg.train()
-    total_loss, total_accuracy, total = 0, 0, 0
-    for i, data in enumerate(train_data_loader):
+    running_loss, running_correct, running_samples = 0, 0, 0
+    for i, (inputs, targets) in enumerate(train_loader):
+        inputs, targets = inputs.to(device), targets.to(device)
         optimizer.zero_grad()
-        X = data[0].to(device)
-        Y = data[1].to(device)
-        y_hat = vgg(X)
-        l = loss(y_hat, Y)
-        l.backward()
+        outputs = vgg(inputs)
+        batch_loss = loss_function(outputs, targets)
+        batch_loss.backward()
         optimizer.step()
+        
+        running_loss += batch_loss * targets.size(0)
+        running_correct += accuracy(outputs, targets)
+        running_samples += targets.size(0)
 
-        with no_grad():
-            total_loss += l * Y.numel()
-            total_accuracy += accuracy(y_hat, Y)
-            total += Y.numel()
-            writer.add_scalar("VGG training loss", total_loss/total, epoch * num_of_batch + i)
-            writer.add_scalar("VGG training accuracy", total_accuracy/total, epoch * num_of_batch + i)
+    train_loss = running_loss / running_samples
+    train_acc = running_correct / running_samples
 
     vgg.eval()
+    test_correct, test_samples = 0, 0
     with no_grad():
-        total_accuracy, total = 0, 0
-        for i, data in enumerate(test_data_loader):
-            X = data[0].to(device)
-            Y = data[1].to(device)
-            total_accuracy += accuracy(vgg(X), Y)
-            total += Y.numel()
-    writer.add_scalar("VGG testing accuracy", total_accuracy/total, epoch)
+        for inputs, targets in test_loader:
+            inputs, targets = inputs.to(device), targets.to(device)
+            outputs = vgg(inputs)
+            test_correct += accuracy(outputs, targets)
+            test_samples += targets.size(0)
+    test_acc = test_correct / test_samples
 
-writer.flush()
+    writer.add_scalars("VGG/Accuracy", {"Train": train_acc, "Test": test_acc}, global_step=epoch)
+    writer.add_scalar("VGG/Loss", train_loss, global_step=epoch)
+    print(f"Epoch [{epoch+1}/{num_epochs}] - Loss: {train_loss:.4f}, Train Acc: {train_acc:.4f}, Test Acc: {test_acc:.4f}")
+
 writer.close()
